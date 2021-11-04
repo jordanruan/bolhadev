1179d4d51e75473122421a8e8f45b90e59557711
Jordan Ruan <jordanruan@hotmail.com>
2021-11-04 09:50:36 -0300

    adicionando Bootstrap nos cards 🎉

--- js/libras.js
+++ js/libras.js
@@ -1,4 +1,6 @@
-< script src = "https://vlibras.gov.br/app/vlibras-plugin.js" > < /script> <
-    script >
-    new window.VLibras.Widget('https://vlibras.gov.br/app'); <
-/script>
+{
+    /* < script src = "https://vlibras.gov.br/app/vlibras-plugin.js" > < /script> <
+        script >
+        new window.VLibras.Widget('https://vlibras.gov.br/app'); <
+    /script> */
+}
